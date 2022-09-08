17b16641b5f3c8cc616692d075dab9bc3815030b
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-09-08 08:32:45 +0600

    primitive and non primitive variable

---
+++ types.js
@@ -0,0 +1,25 @@
+const a = 9;
+const b = 'samsu kupay na ekhon ar.';
+const c = true;
+const age = [20, 40, 30];
+const student = {id: 23, class: 7};
+console.log(typeof a, typeof b, typeof c,   typeof age, typeof student);
+
+
+let x = 5;
+let y = x;
+
+console.log(x, y);
+
+y = 10;
+
+console.log(x, y);
+
+let p = {job: 'web developer'};
+let q = p;
+
+console.log(p, q);
+
+q.job = 'Programmer';
+
+console.log(p, q);
